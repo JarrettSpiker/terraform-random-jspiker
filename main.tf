9a59b213279e52e525f19023c6dd45d91cb757f9
Jarrett Spiker <jarrett.spiker@hashicorp.com>
2023-12-18 15:26:14 -0500

    Add resource 7

--- main.tf
+++ main.tf
@@ -14,6 +14,14 @@ resource "random_id" "random77" {
   byte_length = 8
 }
 
+resource "random_id" "random7" {
+  keepers = {
+    uuid = uuid()
+  }
+
+  byte_length = 8
+}
+
 output "random" {
   value = random_id.random.hex
 }
